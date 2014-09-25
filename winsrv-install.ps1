9a003acd012b87aab61c429ad292db00e32c8041
Joshua Baldwin <iam@joshuabaldwin.eu>
2014-09-25 11:52:32 +0100

    Remove Rubbish

--- winsrv-install.ps1
+++
@@ -1,4 +0,0 @@
-&npm install node-windows
-&npm run-script winsrv-install
-Write-Host "Press any key to continue ..."
-$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
--- winsrv-uninstall.ps1
+++
@@ -1,3 +0,0 @@
-&npm run-script winsrv-uninstall
-Write-Host "Press any key to continue ..."
-$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
